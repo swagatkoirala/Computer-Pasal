bab86e4cb1527ce68f50b634da9c4a2a0932bb6b
jocavinashi <71691615+jocavinashi@users.noreply.github.com>
2021-03-26 12:16:03 +0545

    heroku

--- PcSort/settings.py
+++ PcSort/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = "/image/download/"
 MEDIA_ROOT = BASE_DIR
 
